7b0b352e142c62b7e426789bf9bf249afec58157
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2024-05-05 08:22:27 -0600

    Will it Rain SMS

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
 import requests
 from twilio.rest import Client
 
-api_key = "6add9cc4f2e0549f53281315c849d970"
+api_key = os.environ.get("OWM_API_KEY")
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 account_sid = "AC7c357bb2c70d78979800071781270f39"
-auth_token = "0549b71f9a1e07f77368c2e0bac53485"
+auth_token = os.environ.get("AUTH_TOKEN")
 PARAMETERS = {
     "lat": 47.606209,
     "lon": -122.332069,
@@ -36,12 +37,6 @@ if will_rain:
     )
     print(message.status)
 
-# weather_code = [data["list"][num]["weather"][0]["id"] for num in range(3)]
 
-
-
-# weather = data["list"]
-# print(weather)
-# twelve_hour_rain = {data["list"]["weather"]["id"]: value, for (key, value) in data if id < 700}
 
 
